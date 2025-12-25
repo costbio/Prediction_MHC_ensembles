474f2273bb0471e97236816fdab2c987fc876513
Dilara ONAL <123379299+dilaraonal@users.noreply.github.com>
2025-12-25 22:54:07 +0300

    Delete Thesis directory

--- Thesis/training/annealing.py
+++
@@ -1,6 +0,0 @@
-def kl_beta_schedule(epoch, max_beta, warmup_start=0, warmup_end=60):
-    if epoch <= warmup_start:
-        return 0.0
-    if epoch < warmup_end:
-        return max_beta * (epoch - warmup_start) / (warmup_end - warmup_start)
-    return max_beta
